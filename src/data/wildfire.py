08efc2b3d6f6fbd963e2a9826f4294f76a608fdd
Ashkan Soltanieh <ashkan98@gmail.com>
2022-01-09 13:06:22 -0700

    Implement silver dataset generator script and complete its notebook

--- src/data/wildfire.py
+++ src/data/wildfire.py
@@ -1,46 +1,44 @@
 import geopandas
 import pandas as pd
-import os
 from shapely.wkt import loads
 
-def make_bronze_dataframe(ext_paths, loc_paths):
-    # merging and converiting shp files into data frame
+def make_bronze_dataframes(aob_path, chracteristics_path):
+    # merging and converiting Area of burn and
+    # characteristics shp files into dataframes
     gdf_extents = []
-    for path in ext_paths:
+    for path in aob_path:
         gdf_extents.append(geopandas.GeoDataFrame.from_file(path))
     
     gdf_locations = []
-    for path in loc_paths:
+    for path in chracteristics_path:
         gdf_locations.append(geopandas.GeoDataFrame.from_file(path))
 
-    gdf_ext = pd.concat(gdf_extents, axis = 0)
-    gdf_loc = pd.concat(gdf_locations, axis = 0)
+    gdf_aob = pd.concat(gdf_extents, axis = 0)
+    gdf_characteristics = pd.concat(gdf_locations, axis = 0)
 
     # filter merged data for Alberta and BC
-    gdf_ext_AB_BC = gdf_ext[(gdf_ext["REF_ID"].apply(lambda s : s[0:2] == "AB")) | 
-                            (gdf_ext["REF_ID"].apply(lambda s : s[0:2] == "BC"))]
+    gdf_aob_AB_BC = gdf_aob[(gdf_aob["REF_ID"].apply(lambda s : s[0:2] == "AB")) | 
+                            (gdf_aob["REF_ID"].apply(lambda s : s[0:2] == "BC"))]
 
-    gdf_loc_AB_BC = gdf_loc[(gdf_loc["REF_ID"].apply(lambda s : s[0:2] == "AB")) | 
-                            (gdf_loc["REF_ID"].apply(lambda s : s[0:2] == "BC"))]
+    gdf_characteristics_AB_BC = gdf_characteristics[(gdf_characteristics["REF_ID"].apply(lambda s : s[0:2] == "AB")) | 
+                            (gdf_characteristics["REF_ID"].apply(lambda s : s[0:2] == "BC"))]
 
-    return gdf_ext_AB_BC, gdf_loc_AB_BC
+    return gdf_aob_AB_BC, gdf_characteristics_AB_BC
 
-def make_silver_dataset_csv(extents_path, locations_path):
-    df_ext = pd.read_csv(extents_path, dtype={'UID_Fire': str, 'Year': str}) \
-               .drop(['FD_Agency', 'JD', 'date_src' ,'Map_Date'], axis = 1)
-    
-    # to convert string declaration of polygon into Polygon object
-    df_ext["geometry"] = df_ext["geometry"].apply(lambda shp: loads(shp))
-    
-    # to get area of polygons in km^2
-    df_ext["area"] = df_ext["geometry"].apply(lambda x: (x.area / 10**6))
-    
-    # drop geometry column as it cannot be aggregated
-    df_ext.drop("geometry", axis=1, inplace = True)
-    
-    # aggregated by describe to get all required information for generating distributions
-    df_ext_grp = df_ext.groupby(['UID_Fire', 'Year', 'REF_ID']).describe()
+def make_silver_dataframes(aob_path, characteristics_path):
+    # clean Area of Burn(AoB) data
+    df_aob = pd.read_csv(aob_path, dtype={'UID_Fire': str}) \
+               .drop(['FD_Agency', 'JD', 'date_src', 'Year'], axis = 1)
+    df_aob["geometry"] = df_aob["geometry"].apply(lambda shp: loads(shp))
+    df_aob["area"] = df_aob["geometry"].apply(lambda x: (x.area / 10**6))
+    df_aob.drop("geometry", axis=1, inplace = True)
 
-    # flatten the column labels and revert the indices used in group by back to columns
-    df_ext_grp.columns = ['_'.join(col) for col in df_ext_grp.columns.values]
-    df_ext_grp.reset_index(drop=False, inplace=True)
+    # clean characteristics data
+    df_characteristics = pd.read_csv(characteristics_path, low_memory=False) \
+                           .drop(['FD_Agency', 'dn', 'HHMM', 'sample', 'type', 'geometry'], axis = 1)
+    df_characteristics["YYYYMMDD"] = df_characteristics["YYYYMMDD"] \
+                                        .map(lambda x: str(x)) \
+                                        .map(lambda x: str(x[0:4] + '-' + x[4:6] + '-' + x[6:8]))
+    df_characteristics.rename(columns={'YYYYMMDD': 'Date'}, inplace=True)
+    
+    return df_aob, df_characteristics
