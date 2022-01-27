493aca952bc9520c653cb0cb5ea487778a1574a1
Ashkan Soltanieh <ashkan98@gmail.com>
2022-01-26 22:26:35 -0700

    Implement inferential statistics notebook using final dataset

---
+++ src/utils.py
@@ -0,0 +1,15 @@
+from IPython.display import display, HTML
+
+def display_side_by_side(dfs:list, captions:list):
+    """Display tables side by side to save vertical space
+    Input:
+        dfs: list of pandas.DataFrame
+        captions: list of table captions
+    """
+    output = ""
+    combined = dict(zip(captions, dfs))
+    for caption, df in combined.items():
+        output += df.style.set_table_attributes("style='display:inline'").set_caption(caption)._repr_html_()
+        output += "\xa0\xa0\xa0"
+    
+    display(HTML(output))
